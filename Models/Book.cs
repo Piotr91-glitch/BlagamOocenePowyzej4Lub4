dd263971cee0783171e1cb897b9c975119e75f50
Piotr Pobożniak <piotrpobozniak91@gmail.com>
2024-02-19 21:55:20 +0100

    Dodaj pliki projektów.

---
+++ Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        public int GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required(ErrorMessage = "Release date is required")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 20, ErrorMessage = "Number in stock must be between 1 and 20")]
+        [Required(ErrorMessage = "Number in stock is required")]
+        public byte NumberInStock { get; set; }
+    }
+}
---
+++ SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace LibApp.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            if (!roleManager.RoleExistsAsync("Editor").Result)
+            {
+                var role = new IdentityRole("Editor");
+                var roleResult = roleManager.CreateAsync(role).Result;
+            }
+
+            var userEmail = "Piotr@gmail.com";
+            var user = userManager.FindByEmailAsync(userEmail).Result;
+            if (user != null && !userManager.IsInRoleAsync(user, "Editor").Result)
+            {
+                var result = userManager.AddToRoleAsync(user, "Editor").Result;
+            }
+        }
+    }
+}
